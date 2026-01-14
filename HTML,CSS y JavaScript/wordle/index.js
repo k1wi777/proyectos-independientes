3a793ffdf4e059e3d5d456a83a15b1ad31818e1e
jose padilla <jp3563454@gmail.com>
2026-01-14 03:45:30 -0500

    version 2.0 de mi wordle agregandole un teclado dinamico y varias animaciones desde CSS para hacerlo parecido a la pagina original

--- HTML,CSS y JavaScript/wordle/index.js
+++ HTML,CSS y JavaScript/wordle/index.js
@@ -10,9 +10,13 @@ const palabras = [
 
 let contenedor = document.querySelector("#contenedor");
 let btn = document.querySelector("#btn");
-let form = document.querySelector("#juega");
+let teclado = document.querySelector("#teclado");
+// let form = document.querySelector("#juega");
 let palabraSecreta = "";
 let jugadas = 0;
+let escrito = "";
+let listJugadas = [];
+let correr = false;
 
 function escogerPalabra() {
   let num = Math.floor(Math.random() * palabras.length);
@@ -20,84 +24,207 @@ function escogerPalabra() {
   return palabras[num];
 }
 
-function avanzarJuego(orden) {
-  if (jugadas <= 6) {
-    jugadas++;
-    crearCasillas(orden);
-    return;
-  }
-  contenedor.innerHTML = `<div style="text-align: center;">
-        <h2>se te han acabado los intentos :C</h2>
-        <p>la palabra secreta era :</p>
-        <h3 style="font-size: 20px;">${palabraSecreta}</h3>
-        </div>`;
-  form.classList.toggle("ocultar");
-  btn.classList.toggle("ocultar");
+function crearJuego() {
   jugadas = 0;
-}
-
-function crearCasillas(orden) {
-  if (!orden) {
-    contenedor.innerHTML = ` <div style="text-align: center;">
-            <h2>¡¡HAZ ADIVINADO LA PALABRA!! EXCELENTEE</h2>
-            <p>tu palabra era :</p>
-            <h2><strong>${palabraSecreta}</strong></h2>
-            </div>`;
-    form.classList.toggle("ocultar");
-    btn.classList.toggle("ocultar");
-    jugadas = 0;
-    return;
-  } else if (jugadas <= 6) {
+  escrito = "";
+  for (let index = 0; index < 6; index++) {
     const div = document.createElement("div");
     div.className = "intentos";
-
-    for (let index = 0; index < palabraSecreta.length; index++) {
+    for (let i = 0; i < palabraSecreta.length; i++) {
       const casilla = document.createElement("div");
       casilla.className = "casilla interactiva";
       // casilla.textContent = palabraSecreta[index];
       div.appendChild(casilla);
     }
     contenedor.appendChild(div);
-    return;
   }
-  avanzarJuego(true); // si llega hasta aqui es pq ya se han agotado los intentos y en su ultimo intento no la adivinó
+  listJugadas = contenedor.querySelectorAll(".intentos");
+  listJugadas[0].firstElementChild.classList.toggle("resaltar"); //resalta el primero
+  //   console.log(listJugadas);
 }
 
-function probarPalabra(ingresada, div) {
+function probarPalabra(div) {
   let cuadro = div.firstElementChild;
+  animarSiguiete(cuadro, escrito, 0);
+  jugadas++;
+  avanzar(!(palabraSecreta === escrito));
+  //   avanzarJuego(!(palabraSecreta === escrito));
+}
 
-  for (let index = 0; index < ingresada.length; index++) {
-    let nuevaClase = "incorrecto";
-    // console.log(ingresada[index]);
-    if (palabraSecreta.includes(ingresada[index])) {
-      //si es correcta
-      nuevaClase =
-        ingresada[index] === palabraSecreta[index] ? "correcto" : "existe";
-    }
-    cuadro.classList.add(nuevaClase);
-    cuadro.textContent = ingresada[index];
-    cuadro = cuadro.nextElementSibling;
+function animarSiguiete(elemento, ingresada, index) {
+  //   console.log(index);
+  if (index >= palabraSecreta.length) {
+    return;
   }
-  avanzarJuego(!(palabraSecreta === ingresada));
+
+  let nuevaClase = "incorrecto";
+  // console.log(ingresada[index]);
+  if (palabraSecreta.includes(ingresada[index])) {
+    //si es correcta
+    nuevaClase =
+      ingresada[index] === palabraSecreta[index] ? "correcto" : "existe";
+  }
+
+  elemento.classList.add(nuevaClase);
+  elemento.textContent = ingresada[index];
+
+  let t = teclado.firstElementChild;
+  let valor = "";
+  for (let i = 0; i < 28; i++) {
+    valor = t.textContent.toLowerCase();
+    if (ingresada[index] === valor) {
+      asigna(t, nuevaClase);
+      break;
+    }
+    t = t.nextElementSibling;
+  }
+  //esperamos a que termine la animacion
+  elemento.addEventListener(
+    "animationend",
+    () => {
+      index++;
+      elemento = elemento.nextElementSibling;
+      animarSiguiete(elemento, ingresada, index);
+    },
+    { once: true }
+  );
+}
+
+function asigna(elemento, nuevaClase) {
+  if (!elemento.classList.contains("correcto")) {
+    if (!elemento.classList.contains("incorrecto")) {
+      if (!elemento.classList.contains(nuevaClase)) {
+        elemento.className = `tecla ${nuevaClase}`;
+      }
+    }
+  }
+}
+
+function resalta(actual, otra) {
+  actual.classList.toggle("resaltar");
+  otra.classList.toggle("resaltar");
+}
+function escribir(tecla, fuente) {
+  if (escrito.length < palabraSecreta.length) {
+    escrito += tecla;
+    // console.log(escrito);
+    // console.log(escrito.length);
+    let casilla = listJugadas[jugadas].firstElementChild;
+
+    for (let index = 1; index < escrito.length; index++) {
+      casilla = casilla.nextElementSibling;
+    }
+    if (escrito.length != 0) {
+      escrito.length === palabraSecreta.length
+        ? casilla.classList.toggle("resaltar")
+        : resalta(casilla, casilla.nextElementSibling);
+    }
+    casilla.textContent = tecla;
+    casilla.classList.toggle("prueba");
+    if (fuente === "Backspace") {
+      escrito = escrito.slice(0, -1);
+    }
+  } else if (fuente === "Backspace" && correr) {
+    escrito = escrito.slice(0, -1);
+    let casilla = listJugadas[jugadas].lastElementChild;
+    casilla.textContent = "";
+    casilla.classList.toggle("resaltar");
+  }
+}
+function avanzar(instruccion) {
+  // console.log(`llevas ${jugadas} jugadas`);
+  if (jugadas > 5 && instruccion) {
+    let mensaje = document.createElement("div");
+    mensaje.innerHTML = `<div style="text-align: center;">
+        <h2>se te han acabado los intentos :C</h2>
+        <p>la palabra secreta era :</p>
+        <h3 style="font-size: 20px;">${palabraSecreta}</h3>
+        </div>`;
+    contenedor.appendChild(mensaje);
+    correr = false;
+  } else if (instruccion) {
+    // console.log("avanzando");
+    // jugadas++;
+    escrito = "";
+    listJugadas[jugadas].firstElementChild.classList.toggle("resaltar");
+  } else {
+    let mensaje = document.createElement("div");
+    mensaje.innerHTML = ` <div style="text-align: center;">
+            <h2>¡¡HAZ ADIVINADO LA PALABRA!! EXCELENTEE</h2>
+            <p>tu palabra era :</p>
+            <h2><strong>${palabraSecreta}</strong></h2>
+            </div>`;
+    contenedor.appendChild(mensaje);
+    correr = false;
+  }
+}
+function enter() {
+  if (correr) {
+    if (escrito.length === palabraSecreta.length) {
+      probarPalabra(listJugadas[jugadas]);
+    } else {
+      // console.log("invalido");
+      // console.log(listJugadas[jugadas]);
+      listJugadas[jugadas].classList.toggle("tiembla");
+      listJugadas[jugadas].addEventListener(
+        "animationend",
+        () => {
+          listJugadas[jugadas].classList.toggle("tiembla");
+        },
+        { once: true }
+      );
+      //mostrar la modal 
+      const toast = document.getElementById("toast");
+      toast.classList.add("show");
+      
+      setTimeout(() => {
+        toast.classList.remove("show");
+      }, 2500);
+    }
+  }
 }
 
 btn.addEventListener("click", () => {
   palabraSecreta = escogerPalabra().toLowerCase();
   console.log(palabraSecreta); // para revisar cual fue la palabra
-  form.classList.toggle("ocultar");
+  /*  form.classList.toggle("ocultar");
+   */
+  teclado.classList.toggle("ocultar");
   btn.classList.toggle("ocultar");
   contenedor.innerHTML = "";
-  avanzarJuego(true);
+  correr = true;
+  //   avanzarJuego(true);
+  crearJuego();
 });
 
-form.addEventListener("submit", (evento) => {
-  evento.preventDefault();
-  let ingresado = document.querySelector("#ingresado").value.toLowerCase();
+teclado.addEventListener("click", (evento) => {
+  if (evento.target.classList.contains("tecla")) {
+    // console.dir(evento.target.textContent.toLowerCase());
 
-  if (ingresado.length !== palabraSecreta.length) {
-    alert("debes ingresar una palabra con el mismo numero de letras ");
-    return;
+    // console.log(evento.target.id);
+    if (evento.target.id !== "enter") {
+      let fuente = evento.target.id === "borrar" ? "Backspace" : "";
+      let letra =
+        evento.target.id === "borrar"
+          ? ""
+          : evento.target.textContent.toLowerCase();
+      escribir(letra, fuente);
+    } else {
+      enter();
+    }
   }
-  //   console.log(contenedor.lastElementChild);
-  probarPalabra(ingresado, contenedor.lastElementChild);
 });
+
+// encargado de detectar tus pulsasiones
+addEventListener("keydown", (evento) => {
+  let fuente = evento.key;
+  let tecla = evento.key.length > 1 ? "" : evento.key;
+  //   console.log(tecla);
+
+  if (evento.key === "Enter") {
+    enter();
+  } else {
+    escribir(tecla, fuente);
+  }
+});
+
